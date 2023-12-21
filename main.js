17f8c55936978d410b09c1109bd97cb21a562cfd
방호진 <banghojin@banghojin-ui-MacBookAir.local>
2023-12-21 15:44:15 +0900

    a

--- main.js
+++ main.js
@@ -5,20 +5,13 @@ import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = /* html */`
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>안뇽</h1>
+    <h1>강현모 평택 복귀팩 </h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+    쇼쿠지 - 와일드그라스 - 피시방 - 폐계닭 - 하루마침 - 오네르 - 젠누리버거 -블랙택시 - 탄광맥주 - 아이스크림
     </p>
   </div>
 `
 
-setupCounter(document.querySelector('#counter'))
